e620ede7d2664799d48d4393a13c60ad6d29be8f
Flavio Cesar(Web-Programmer) <119898588+FlavioC30@users.noreply.github.com>
2024-06-25 12:57:16 +0000

    commirgcgfg

--- clase1.2.py
+++ clase1.2.py
@@ -1,19 +1,64 @@
-import os
-
-# Crear el directorio 'streamlit_hello'
-os.makedirs('streamlit_hello', exist_ok=True)
-
-# Cambiar al directorio 'streamlit_hello'
-os.chdir('streamlit_hello')
-
-## 3
-with open('hello_demo.py', 'w') as f:
-    f.write("""
-import streamlit as st
-st.write('Hello World')
-""")
-
-## 4
+# hello_demo.py
 
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import altair as alt
+import seaborn as sns
+
+# Sección 1: Hola Mundo
 st.write('Hello World')
+
+# Sección 2: Media de una distribución binomial
+binom_dist = np.random.binomial(1, 0.5, 100)
+st.write(np.mean(binom_dist))
+
+# Sección 3: Histograma de medias
+binom_dist = np.random.binomial(1, 0.5, 1000)
+list_of_means = [np.random.choice(binom_dist, 100, replace=True).mean() for _ in range(1000)]
+fig, ax = plt.subplots()
+ax.hist(list_of_means)
+st.pyplot(fig)
+
+# Sección 4: Histograma con múltiples gráficos
+fig1, ax1 = plt.subplots()
+ax1.hist(list_of_means)
+st.pyplot(fig1)
+
+fig2, ax2 = plt.subplots()
+ax2.hist([1, 1, 1, 1])
+st.pyplot(fig2)
+
+# Sección 5: Entrada del usuario para la probabilidad de cara en una moneda
+perc_heads = st.number_input(label='Posibilidad de que las monedas caigan en cara', min_value=0.0, max_value=1.0, value=0.5, key='number_input_1')
+binom_dist = np.random.binomial(1, perc_heads, 1000)
+list_of_means = [np.random.choice(binom_dist, 100, replace=True).mean() for _ in range(1000)]
+fig, ax = plt.subplots()
+ax.hist(list_of_means, range=[0, 1])
+st.pyplot(fig)
+
+# Sección 6: Título y descripción de la aplicación
+st.title('Ilustrando el teorema del límite central con Streamlit')
+st.subheader('Una aplicación de Tyler Richards')
+st.write('Esta aplicación simula mil lanzamientos de monedas utilizando la posibilidad de que salga cara ingresada a continuación, y luego toma muestras con reemplazo de esa población y traza el histograma de las medias de las muestras para ilustrar el teorema del límite central.!')
+
+# Reutilizando entrada del usuario y cálculo anterior
+fig, ax = plt.subplots()
+ax.hist(list_of_means)
+st.pyplot(fig)
+
+# Sección 7: Palmer's Penguins
+st.title("Palmer's Penguins")
+penguins_df = pd.read_csv('penguins.csv')
+st.write(penguins_df.head())
+
+# Sección 8: Gráfico de dispersión personalizado
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
+selected_species = st.selectbox('What species would you like to visualize?', ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st.selectbox('What do you want the x variable to be?', ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+
+penguins_df = penguins_df[penguins_df['species'] == selected_species]
+alt_chart = alt.Chart(penguins_df).mark_circle().encode(x=selected_x_var, y=selected_y_var)
+st.altair_chart(alt_chart)
--- clase1.3.py
+++ clase1.3.py
@@ -1,111 +1,84 @@
-# agregar texto
+import streamlit as st
+from PIL import Image
+import datetime
+import time
+import os
 
-	st.header("This is a header")
-	st.subheader("This is a subheader")
-	st.text("This is a simple text")
-	st.write("This is a write dimension")
+# Agregar texto
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.text("This is a simple text")
+st.write("This is a write dimension")
 
-# hipervinculos
-	“st.markdown("[Streamlit](https://www.streamlit.io)")”
+# Hipervínculos
+st.markdown("[Streamlit](https://www.streamlit.io)")
+st.markdown("https://www.streamlit.io")
 
+html_page = """
+<div style="background-color:blue;padding:50px">
+<p style="color:yellow;font-size:50px">Enjoy Streamlit!</p>
+</div>
+"""
+st.markdown(html_page, unsafe_allow_html=True)
 
-	st.markdown("https://www.streamlit.io")
+# Colores en textos
+st.success("Success!")
+st.info("Information")
+st.warning("This is a warning!")
+st.error("This is an error!")
 
-	“html_page = """
-	<div style="background-color:blue;padding:50px">
-	<p style="color:yellow;font-size:50px">Enjoy Streamlit!</p>
-	</div>
-	"""
-	st.markdown(html_page, unsafe_allow_html=True)”
-
-#Colored textboxes
-
-	“st.success("Success!")
-	st.info("Information")
-	st.warning("This is a warning!")
-	st.error("This is an error!")”
-
-# “Images, audio, and video”
-
-	“from PIL import Image
-	img = Image.open("packt.jpeg")
-	st.image(img, width=300, caption="Packt Logo")”
-
-	“video_file = open("SampleVideo_1280x720_1mb.mp4","rb")
-	video_bytes = video_file.read()
-	st.video(video_bytes)”
-
-
-# streamlit Widgets
-
-“if st.button("Play"):
-	st.text("Hello world!")”
-
-“if st.checkbox("Checkbox"):
-	st.text("Checkbox selected")”
-
-“radio_but = st.radio("Your Selection", ["A", "B"])
-if radio_but == "A":
-	st.info("You selected A")
+# Imágenes, audio y video
+image_path = "packt.jpeg"
+if os.path.exists(image_path):
+    img = Image.open(image_path)
+    st.image(img, width=300, caption="Packt Logo")
 else:
-	st.info("You selected B")”
+    st.error("Image file not found")
 
-“city = st.selectbox("Your City", ["Napoli", "Palermo", "Catania"])”
+video_path = "SampleVideo_1280x720_1mb.mp4"
+if os.path.exists(video_path):
+    video_file = open(video_path, "rb")
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+else:
+    st.error("Video file not found")
 
-# multiselect
-“occupation = st.multiselect("Your Occupation", ["Programmer", "Data Scientist", "IT Consultant", "DBA"])”
+# Widgets de Streamlit
+if st.button("Play"):
+    st.text("Hello world!")
 
+if st.checkbox("Checkbox"):
+    st.text("Checkbox selected")
 
-“Inputting text and numbers”
+radio_but = st.radio("Your Selection", ["A", "B"])
+if radio_but == "A":
+    st.info("You selected A")
+else:
+    st.info("You selected B")
 
-“Name = st.text_input("Your Name", "Write something…")
-st.text(name)”
+city = st.selectbox("Your City", ["Napoli", "Palermo", "Catania"])
 
-“Age = st.number_input("Input a number")”
+# Multiselect
+occupation = st.multiselect("Your Occupation", ["Programmer", "Data Scientist", "IT Consultant", "DBA"])
 
+# Ingresando texto y números
+name = st.text_input("Your Name", "Write something…")
+st.text(name)
 
-“message = st.text_area("Your Message", "Write something...")”
+age = st.number_input("Input a number")
+
+message = st.text_area("Your Message", "Write something...")
 
 # Slider
+select_val = st.slider("Select a Value", 1, 10)
 
-“select_val = st.slider("Select a Value", 1, 10)”
+# Globos
+if st.button("Balloons"):
+    st.balloons()
 
-# Balloons
-“if st.button("Balloons"):
-	st.balloons()”
+# Fecha, hora y más
+today = st.date_input("Today is", datetime.datetime.now())
+hour = st.time_input("The time is", datetime.time(12, 30))
 
-# “Date, time, and more”
-
-“import datetime
-today = st.date_input("Today is",datetime.datetime.now())”
-“import time
-hour = st.time_input("The time is",datetime.time(12,30))”
-
-“data = {"name":"John","surname":"Wick"}
-st.json(data)”
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+data = {"name": "John", "surname": "Wick"}
+st.json(data)
